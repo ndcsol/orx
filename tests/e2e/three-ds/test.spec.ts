b6a310984a2a8ffd4006d0d4af19878ba59c2014
parsasi <mail@parsa.pro>
2025-02-28 15:53:35 -0800

    changed the wait time to 6s to see if it solves the timeout issue

--- tests/e2e/three-ds/test.spec.ts
+++ tests/e2e/three-ds/test.spec.ts
@@ -51,7 +51,7 @@ test.describe('Proper handling of the stripe modal', () => {
     const errorElement = await page.waitForSelector("#error", {
       state: 'attached'
     });
-    await page.waitForTimeout(3500);
+    await page.waitForTimeout(6000);
     expect(successElement.textContent()).resolves.toBeTruthy();
     expect(errorElement.textContent()).resolves.toBeFalsy();
 
