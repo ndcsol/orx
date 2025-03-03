81658a0d6f3162283b95023871e11a906683c8ab
parsasi <mail@parsa.pro>
2025-03-03 10:56:54 -0800

    Handling known errors as error outputs instead of successful ones

--- tests/e2e/three-ds/main.js
+++ tests/e2e/three-ds/main.js
@@ -25,21 +25,27 @@ function urlParamsToObject() {
   return queryParams;
 }
 
-function createResultElement(id) {
+function createResultElement(id, content) {
   const element = document.createElement("pre");
   element.id = id;
+  element.innerText = content;
   return element;
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
   const threeDsPayload = urlParamsToObject();
   threeDs(threeDsPayload).then(results => {
-    const successElement = createResultElement('success');
-    successElement.innerText = JSON.stringify(results, null, 2);
-    document.getElementsByTagName("body")[0].appendChild(successElement);
+    const parsedResponse = JSON.stringify(results, null, 2);
+    if (results.status === 'success') {
+      const successElement = createResultElement('success', parsedResponse);
+      document.getElementsByTagName("body")[0].appendChild(successElement);
+    } else {
+      const errorElement = createResultElement('error', parsedResponse);
+      document.getElementsByTagName("body")[0].appendChild(errorElement);
+    }
   }).catch((error) => {
-    const errorElement = createResultElement('error');
-    errorElement.innerText = JSON.stringify(error, null, 2);
+    const parsedResponse = JSON.stringify(error, null, 2);
+    const errorElement = createResultElement('error', parsedResponse);
     document.getElementsByTagName("body")[0].appendChild(errorElement);
   })
 });
